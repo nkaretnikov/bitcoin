f80974ea3dd45ca64241642cfe1b4b506d056d16
Nikita Karetnikov <nikita@karetnikov.org>
2026-05-25 19:40:37 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with import <nixpkgs> {};
+
+mkShell {
+  packages = [
+    cmake
+    llvmPackages_latest.clang
+    llvmPackages_latest.lld
+    llvmPackages_latest.llvm
+    sqlite
+    capnproto
+    boost
+    pkg-config
+    libevent
+    python3
+  ];
+
+  shellHook = ''
+    export CC=clang
+    export CXX=clang++
+  '';
+}
+
