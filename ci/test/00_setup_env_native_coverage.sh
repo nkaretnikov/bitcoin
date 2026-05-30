d92591c71883993c94106a98ba2f4d542319597c
Nikita Karetnikov <nikita@karetnikov.org>
2026-05-30 10:27:55 +0000

    Reuse ccache actions

---
+++ ci/test/00_setup_env_native_coverage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_coverage
